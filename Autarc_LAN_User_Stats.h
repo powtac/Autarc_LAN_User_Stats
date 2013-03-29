c8cec4a71003ee87a89596a79eeba0906ff3b0ec
Simon Brüchner <powtac@gmx.de>
2013-03-29 12:32:07 +0100

    add templates

---
+++ Autarc_LAN_User_Stats.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef Autarc_LAN_User_Stats_H_
+#define Autarc_LAN_User_Stats_H_
+#include "Arduino.h"
+//add your includes for the project Autarc_LAN_User_Stats here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Autarc_LAN_User_Stats here
+
+
+
+
+//Do not add code below this line
+#endif /* Autarc_LAN_User_Stats_H_ */
