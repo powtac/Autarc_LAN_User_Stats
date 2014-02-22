307e41faf47d139d55d6332bc9ebc25c3454b6b5
powtac <powtac@gmx.de>
2014-02-22 16:24:20 +0100

    Clean up

--- server/mysql.php
+++ server/mysql.php
@@ -1,9 +1,8 @@
 <?php
+
 header('content-type: text/html;charset=utf-8');
 
 mysql_connect('Server', 'Username', 'Password') or die (mysql_error());
 mysql_select_db('danit_lan-user');
 mysql_query('SET NAMES "utf8"');
 mysql_set_charset("utf8");
-
-?>
