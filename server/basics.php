4cc6284025c6b20e8b01828d894e3b9f5ea29f1c
powtac <powtac@gmx.de>
2014-02-22 16:06:28 +0100

    Create basics.php
    Initially adding function to format MAC addresses for display.

---
+++ server/basics.php
@@ -0,0 +1,15 @@
+<?php
+
+function mac_format($mac, $separator = ':', $uppercase = TRUE) {
+    if (is_array($mac)) {
+        $mac = implode($separator, $mac);
+    } else {
+        $mac = str_replace(array(':', '.', '-', '_', ' ', ','), $separator, $mac);
+    }
+    
+    if ($uppercase) {
+        $mac = ucfirst($mac);
+    }
+    
+    return $mac;
+}
