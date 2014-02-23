4c62591d3f8a355b5438b01b1944eb3e9042cb64
powtac <powtac@gmx.de>
2014-02-23 12:44:22 +0100

    Clean up

--- autarc_lan_user_stats/IPHelper.h
+++ autarc_lan_user_stats/IPHelper.h
@@ -1,6 +1,5 @@
 // Some helpers for TCP/IP stuff
 
-// See http://playground.arduino.cc/Main/Printf
 #include <stdarg.h>
 #include "memcheck.h"
   //Serial.println(get_mem_unused());
@@ -17,7 +16,7 @@ void print_ip(byte* ip) {
 
 
 void print_mac(byte* mac) {
-  Serial.print(mac[0], HEX);
+  Serial.print(mac[0]);
   Serial.print(":");
   Serial.print(mac[1], HEX);
   Serial.print(":");
@@ -40,62 +39,62 @@ void send_info_to_server(byte* startip, PtrArray maclist, char* AVRID) {
   //delay(1000);
   Serial.print("Speicher (send_info): ");
   Serial.println(get_mem_unused());
-    EthernetClient client;
-      //IPAddress server(85, 10, 211, 16); // kolchose.org 
-       IPAddress server(85, 214, 144, 114); //lan-user.danit.de
-      
-      //int ret = client.connect("kolchose.org", 80);
-       int ret = client.connect("lan-user.danit.de", 80);
-      if (ret == 1) {
-        Serial.println("Connected to HTTP Server");
+  EthernetClient client;
+  //IPAddress server(85, 10, 211, 16); // kolchose.org 
+  IPAddress server(85, 214, 144, 114); //lan-user.danit.de
+  
+  //int ret = client.connect("kolchose.org", 80);
+  int ret = client.connect("lan-user.danit.de", 80);
+  if (ret == 1) {
+    Serial.println("Connected to HTTP Server");
 
-        // Make a HTTP request:
-        //client.print("GET /autarc_lan_user_stats/"); // kolchose.org 
-         client.print("GET /"); // lan-user.danit.de
-        client.print("?AVR_ID=");
-        client.print(AVRID);
-      Serial.println(AVRID);
-      //HTTP String:  AVR_ID=AVR_ID&IP[]=Ip1&MAC[]=Mac2&IP[]=IP2&MAC[]=Mac2
-        for(int z = 0; z < 35; z++) {  //Todo: Set to 255
-          //if (maclist[z] [5] > 0) {    //Todo: Richtig filtern!
-            client.print("&IP[]=");
-            client.print(startip[0]);
-            client.print(".");
-            client.print(startip[1]);
-            client.print(".");
-            client.print(startip[2]);
-            client.print(".");
-            client.print(z);
-            client.print("&MAC[]=");
-            client.print(maclist[z][0]);
-            client.print(".");
-            client.print(maclist[z][1], HEX);
-            client.print(".");
-            client.print(maclist[z][2], HEX);
-            client.print(".");
-            client.print(maclist[z][3], HEX);
-            client.print(".");
-            client.print(maclist[z][4], HEX);
-            client.print(".");
-            client.print(maclist[z][5], HEX);
-          //}
-        }
+    // Make a HTTP request:
+    //client.print("GET /autarc_lan_user_stats/"); // kolchose.org 
+    client.print("GET /"); // lan-user.danit.de
+    client.print("?AVR_ID=");
+    client.print(AVRID);
+    Serial.println(AVRID);
+    //HTTP String:  AVR_ID=AVR_ID&IP[]=Ip1&MAC[]=Mac2&IP[]=IP2&MAC[]=Mac2
+    for(int z = 0; z < 35; z++) {  //Todo: Set to 255
+      //if (maclist[z] [5] > 0) {    //Todo: Richtig filtern!
+        client.print("&IP[]=");
+        client.print(startip[0]);
+        client.print(".");
+        client.print(startip[1]);
+        client.print(".");
+        client.print(startip[2]);
+        client.print(".");
+        client.print(z);
+        client.print("&MAC[]=");
+        client.print(maclist[z][0], HEX);
+        client.print(".");
+        client.print(maclist[z][1], HEX);
+        client.print(".");
+        client.print(maclist[z][2], HEX);
+        client.print(".");
+        client.print(maclist[z][3], HEX);
+        client.print(".");
+        client.print(maclist[z][4], HEX);
+        client.print(".");
+        client.print(maclist[z][5], HEX);
+      //}
+    }
 
-        client.println(" HTTP/1.0");
-        //client.println("Host: kolchose.org");           // Important! TODO check if this is required and dynamically asignable
-        client.println("Host: lan-user.danit.de");
-        client.println("User-Agent: Autarc_LAN_User_Stats"); // Important!
-        client.println();        // Important!
-        
-      Serial.println(client.status());
-      client.stop();
+    client.println(" HTTP/1.0");
+    //client.println("Host: kolchose.org");           // Important! TODO check if this is required and dynamically asignable
+    client.println("Host: lan-user.danit.de");
+    client.println("User-Agent: Autarc_LAN_User_Stats"); // Important!
+    client.println();        // Important!
+    
+    Serial.println(client.status());
+    client.stop();
 
-      }
-      else {
-      Serial.println("NOT connected to HTTP Server");
-      Serial.println(ret);
-      Serial.println("\n");
-      Serial.println(client.status());
-      client.stop();
+  }
+  else {
+    Serial.println("NOT connected to HTTP Server");
+    Serial.println(ret);
+    Serial.println("\n");
+    Serial.println(client.status());
+    client.stop();
   } 
 }
