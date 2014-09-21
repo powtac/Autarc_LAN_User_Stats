857678ffcabebc55c57cdcfea6a0e967a547288b
powtac <powtac@gmx.de>
2014-09-21 13:48:24 +0200

    Clean up

--- server/index.php
+++ server/index.php
@@ -3,12 +3,10 @@
 require_once dirname(__FILE__).'/basics.php';
 require_once dirname(__FILE__).'/mysql.php';
 
-header('content-type: text/html;charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
 
-$output = '<?xml version="1.0" encoding="utf-8" ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
+$output = '<!DOCTYPE html>
+<html>
     <head>
         <title>Autarc-Lan-User-Stat</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -89,10 +87,6 @@ if (isset($_GET['AVR_ID'])) {
             $output .= "<tr><td>".$row['IP']."</td><td><a href=\"http://www.coffer.com/mac_find/?string=".urlencode(mac_format($row['MAC'])).""\" target=\"_blank\">".mac_format($row['MAC'])."</a></td><td>".(date("j" , $timediff) - 1)." Tage ".(date("G" , $timediff) - 1)." h ".date("i" , $timediff)." min ".date("s" , $timediff)." sec</td><td>".date("Y-m-d - G:i" , $row['last_scan'])." Uhr</td></tr>";
         }
         $output .= '</table>';
-	$output .= '<a href="https://github.com/powtac/Autarc_LAN_User_Stats/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://github-camo.global.ssl.fastly.net/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>';
-	$output .= "</body>
-    </html>";
-	echo $output;
     }
 
 }
@@ -111,12 +105,11 @@ else {
             $output .= '<option value="'.$row['AVR_ID'].'">&nbsp;'.$row['AVR_ID'].'&nbsp;</option>';
         }
     $output .= '</select><br /><br /><input type="submit" name="cmdShow" value="Anzeigen"/></form>';
-    $output .= '<a href="https://github.com/powtac/Autarc_LAN_User_Stats/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://github-camo.global.ssl.fastly.net/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>';
-    $output .= "</body>
-    </html>";
-    echo $output;
 }
 
+$output .= '<a href="https://github.com/powtac/Autarc_LAN_User_Stats/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://github-camo.global.ssl.fastly.net/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>';
+echo $output;
+echo "</body></html>";
 
 /* Save this request */
 /* $fp = fopen("req.txt","w"); 	overwrite last request */
