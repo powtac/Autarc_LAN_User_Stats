1d76dd398149e571fc8031c080e1af68c325eb73
Simon Brüchner <powtac@gmx.de>
2015-06-05 23:21:52 +0200

    refs #56 fix for URL and Content-Type
    This works!!!! :-)

--- autarc_lan_user_stats/autarc_lan_user_stats.ino
+++ autarc_lan_user_stats/autarc_lan_user_stats.ino
@@ -55,8 +55,8 @@ byte countOfflineDevices = 0;
 //char serverURL[] = "lan-user.danit.de";
 char serverURL[] = "kolchose.org";
 //char serverPath[] = "";
-char serverPath[] = "/autarc_lan_user_stats/03/?";
-char VersionNR[] = "1.2";  //TODO: Automatically?
+char serverPath[] = "/autarc_lan_user_stats/03/\?";
+char VersionNR[] = "1.3";  //TODO: Automatically?
 #define MAX_DEVICES_INFO 5
 
 byte offlineIP[MAX_DEVICES_INFO][4];
@@ -932,10 +932,10 @@ char send_info_to_server(char *name) {
     client.print(F("User-Agent: Autarc_LAN_User_Stats "));
     client.println(VersionNR);
     client.println(F("Connection: close"));
-    client.println(F("Content-Type: application/x-www-form-urlencoded;"));
-    //client.println(F("Content-Type: application/json;"));
+    // client.println(F("Content-Type: application/x-www-form-urlencoded;"));
+    client.println(F("Content-Type: application/json; charset=UTF-8"));
     client.print(F("Content-Length: "));
-    client.println("300"); //TODO: Maybe calculate this later..?
+    client.println("105"); //TODO: Maybe calculate this later..?
     client.println(); // Important!
 
     client.print("{");
