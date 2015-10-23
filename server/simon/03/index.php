5f6ea5ab893abac03218b7d692ee3e66d366c012
Tim <pctim1@web.de>
2015-10-23 18:46:43 +0200

    bugfix accept content type at server

--- server/simon/03/index.php
+++ server/simon/03/index.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 require_once dirname(__FILE__).'/config/config.php';
 require_once dirname(__FILE__).'/config/bootstrap.php';
